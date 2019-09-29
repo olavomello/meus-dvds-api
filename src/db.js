51d22ba0d1307f54e7a8c8a2e3dcb824fa0e14bd
Olavo Mello <olavo@agenciadix.com.br>
2019-09-29 10:22:25 -0300

    Connection timeout handler added

--- src/db.js
+++ src/db.js
@@ -1,21 +1,35 @@
 ﻿const mysql =   require("mysql");
-const conn  =   mysql.createConnection({
-    host        :   "",
-    port        :   3306,
-    user        :   "",
-    password    :   "",
-    database    :   "dvds"
-});
+// Configs DB Connection
+var db_config = {
+    host        : '68.233.247.70',
+    user        : 'dvds',
+    password    : 'vGN@PhG1IId39nO',
+    database    : 'dvds',
+    port        : 3306
+  };
 
-try{
-    conn.connect( (err) =>{
-        if( err )
-        console.error("DB error : " + err );
-    });
-} catch(ex){}
+var conn;
 
-conn.on('error', (err) =>{
-    console.error("Conn error : " + err );
-});
+function handleDisconnect() {
+  conn = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
 
+  conn.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  conn.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+// Start
+handleDisconnect();
 module.exports = conn;
